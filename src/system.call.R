1febd52d77dd94800a6fc71c02118c041ad7c272
edugp <edugarpor@gmail.com>
2016-03-31 20:23:15 +0200

    Update system.call.R

--- src/system.call.R
+++ src/system.call.R
@@ -3,6 +3,7 @@ setwd("/Path/to/cpp/binaries")
 
 # Parameters
 t <- 1
+time <- rep(1, 2)
 mu <- c(0, -0.57)
 alpha <- c(1, 2, 0)
 sigma <- c(1, 1.0)
@@ -12,17 +13,27 @@ N <- 10
 maxK <- 1
 etrunc <- 25
 
-# Call tpd
-loglik <- as.double(system(paste("./maintpd", t, paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), maxK, etrunc, paste(as.vector(t(pairs)), collapse = " ")), intern = TRUE))
+## First versions
+
+# Call logliktime
+loglik <- as.double(system(paste("./loglik", t, paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), maxK, etrunc, paste(as.vector(t(pairs)), collapse = " ")), intern = TRUE))
 loglik
 
-# Call sample
-samp <- system(paste("./mainsamp", N, paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), paste(as.vector(t(pairs)), collapse = " ")), intern = TRUE)
+# Call sampstat
+samp <- system(paste("./sampstat", N, paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), paste(as.vector(t(pairs)), collapse = " ")), intern = TRUE)
 samp <- matrix(scan(text = samp, what = numeric(), quiet = TRUE), nrow = N, ncol = 4, byrow = TRUE)
 samp
 
-# Call dens
-dens <- system(paste("./maindens", paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), maxK, etrunc, paste(as.vector(t(x)), collapse = " ")), intern = TRUE)
+# Call samptrans
+dens <- system(paste("./samptrans", paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), maxK, etrunc, paste(as.vector(t(x)), collapse = " ")), intern = TRUE)
 dens <- scan(text = dens, what = numeric(), quiet = TRUE)
 dens
+
+## Newer versions with vectorized times - C++ 11 dependent!
+
+# Call logliktime
+loglik <- as.double(system(paste("./logliktime", paste(mu, collapse = " "), paste(alpha, collapse = " "), paste(sigma, collapse = " "), maxK, etrunc, paste(as.vector(t(pairs)), collapse = " "), paste(time, collapse = " ")), intern = TRUE))
+loglik
+
+
 
